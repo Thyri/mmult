04112a1fb5d5fadb0c9b6881801e61da068dbcd9
Max Maurin <mxm4158@cs.rit.edu>
2016-10-27 13:18:12 -0400

    Stubbed out mmult.c

---
+++ mmult.c
@@ -0,0 +1,30 @@
+// // // // // // // // // // // // // //
+/// File: mmult.c
+/// Description: helper functions for project 2
+/// @author: Max Maurin <csci243>
+// // // // // // // // // // // // // //
+
+#include "mmult.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+long get_count(){
+	return 0;
+}
+
+long get_dimensions(){
+	return 0;
+}
+
+void fill_matrix(){
+	return;
+}
+
+void multiply_matrix(){
+	return;
+}
+
+void print_matrix(){
+	return;
+}
