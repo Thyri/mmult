7b279584358365dbf4b039d7980a3f7a0f24d4ff
Max Maurin <mxm4158@cs.rit.edu>
2016-10-27 13:12:41 -0400

    mmult-driver.c is now capable of compiling (simply returns 0) and mmult.h is stubbed out

--- mmult-driver.c
+++ mmult-driver.c
@@ -1,3 +1,9 @@
+// // // // // // // // // // // // // //
+/// File: mmult-driver.c
+/// Description: The main file for project 2 in Mechanics of Programming
+/// @author: Max Maurin <csci243>
+// // // // // // // // // // // // // //
+
 int main(int argc, char *argv[]){
 	
 	return 0;
---
+++ mmult.h
@@ -0,0 +1,23 @@
+// // // // // // // // // // // // // //
+/// File: mmult.h
+/// Description: header file for all
+//	helper functions in mmult.c
+/// @author: Max Maurin <csci243>
+// // // // // // // // // // // // // //
+
+#ifndef MMULT_H
+#define MMULT_H
+#define NUL     '\0'
+#define MAXLINE 80
+
+long get_count();
+
+long get_dimensions();
+
+void fill_matrix();
+
+void multiply_matrix();
+
+void print_matrix();
+
+#endif
